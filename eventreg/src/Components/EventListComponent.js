2cb66702a49c69ee09c83dadd58d0295e598994b
Vbj <ijalab1808@gmail.com>
2020-06-07 09:26:57 +0530

    added something

--- eventreg/src/Components/EventListComponent.js
+++ eventreg/src/Components/EventListComponent.js
@@ -1,10 +1,32 @@
 import React , { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { Row, Col } from 'reactstrap';
+import { Jumbotron } from 'reactstrap';
 import EventCard from './EventCardComponent';
 import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import "../App.css";
 
+const Jumbo = () => {
+    return(
+        <Jumbotron className="jumbotron">
+            <div className="container">
+                <div className="row row-header">
+                <div className="col-12 col-sm-1">
+                        <i className="fa fa-dashboard fa-3x " aria-hidden="true" ></i>
+                        
+                    </div>
+                    
+                <div className="col-12 col-sm-11">
+                        <h1>Dashboard</h1>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </Jumbotron>
+    )
+}
 
 class EventList extends Component{
     constructor(props){
@@ -45,23 +67,25 @@ class EventList extends Component{
             <div>
             <div>
                 <Header />
+                <Jumbo />
             </div>
             <div className="EventList">
                 
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <br />
-                        <h2 className="display-4 text-center">Events List</h2>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <br />
+                            <h2 className="display-4 text-center"><i className="fa fa-list " aria-hidden="true" style={{marginRight: '7px'}}></i>Events List</h2>
 
+                        </div>
+                    </div>
+                    <div className="list">
+                    
+                        {eventList}
                     </div>
                 </div>
-                <div className="list">
-                    
-                    {eventList}
-                </div>
             </div>
-            </div>
+            <Footer />
             </div>
         );
         
